08ab97fb38fde20c041f8e7d3be4a71d259b23fd
LastStap <dumshenko2003@gmail.com>
2024-11-18 09:15:55 +0100

    small code corrections

--- src/test/java/dumshenko/daniil/ShapeTest.java
+++ src/test/java/dumshenko/daniil/ShapeTest.java
@@ -10,36 +10,39 @@ public class ShapeTest {
     public void shouldCorrectlyComputeCircleArea() {
         // Given
         Circle circle = new Circle(ShapeType.CIRCLE, 10);
-
-        // When
         double expectedArea = Math.PI * 10 * 10;
 
+        // When
+        double actualArea = circle.calculateArea();
+
         // Then
-        assertEquals(expectedArea, circle.calculateArea(), 0.0001);
+        assertEquals(expectedArea, actualArea, 0.0001);
     }
 
     @Test
     public void shouldCorrectlyComputeRectangleArea() {
         // Given
         Rectangle rectangle = new Rectangle(ShapeType.RECTANGLE, 5, 6);
-
-        // When
         double expectedArea = 5 * 6;
 
+        // When
+        double actualArea = rectangle.calculateArea();
+
         // Then
-        assertEquals(expectedArea, rectangle.calculateArea(), 0.0001);
+        assertEquals(expectedArea, actualArea, 0.0001);
     }
 
     @Test
     public void shouldCorrectlyComputeTriangleArea() {
         // Given
         Shape triangle = new Triangle(ShapeType.TRIANGLE, 6, 4, 4);
-
-        // When
         double expectedArea = Math.sqrt(7 * (7 - 6) * (7 - 4) * (7 - 4));
 
+        // When
+        double actualArea = triangle.calculateArea();
+
         // Then
-        assertEquals(expectedArea, triangle.calculateArea(), 0.0001);
+        assertEquals(expectedArea, actualArea, 0.0001);
     }
 
 
@@ -47,59 +50,64 @@ public class ShapeTest {
     public void shouldCorrectlyComputeSquareArea() {
         // Given
         Shape square = new Square(ShapeType.SQUARE, 5);
-
-        // When
         double expectedArea = 5 * 5;
 
+        // When
+        double actualArea = square.calculateArea();
+
         // Then
-        assertEquals(expectedArea, square.calculateArea(), 0.0001);
+        assertEquals(expectedArea, actualArea, 0.0001);
     }
 
     @Test
     public void shouldCorrectlyComputeSquarePerimeter() {
         //Given
         Shape square = new Square(ShapeType.SQUARE, 5);
-
-        //When
         double expectedPerimeter = (5 * 2) + (5 * 2);
 
+        //When
+        double actualPerimeter = square.calculatePerimeter();
+
         //Then
-        assertEquals(expectedPerimeter, square.calculatePerimeter(), 0.0001);
+        assertEquals(expectedPerimeter, actualPerimeter, 0.0001);
     }
 
     @Test
     public void shouldCorrectlyComputeRectanglePerimeter() {
         //Given
         Shape rectangle = new Rectangle(ShapeType.RECTANGLE, 5, 6);
-
-        //When
         double expectedPerimeter = (5 * 2) + (6 * 2);
 
+        //When
+        double actualPerimeter = rectangle.calculatePerimeter();
+
         //Then
-        assertEquals(expectedPerimeter, rectangle.calculatePerimeter(), 0.0001);
+        assertEquals(expectedPerimeter, actualPerimeter, 0.0001);
     }
 
     @Test
     public void shouldCorrectlyComputeCirclePerimeter() {
         //Given
         Shape circle = new Circle(ShapeType.CIRCLE, 10);
-
-        //When
         double expectedPerimeter = 2 * Math.PI * 10;
 
+        //When
+        double actualPerimeter = circle.calculatePerimeter();
+
         //Then
-        assertEquals(expectedPerimeter, circle.calculatePerimeter(), 0.0001);
+        assertEquals(expectedPerimeter, actualPerimeter, 0.0001);
     }
 
     @Test
     public void shouldCorrectlyComputeTrianglePerimeter() {
         //Given
         Shape triangle = new Triangle(ShapeType.TRIANGLE, 6, 3, 4);
-
-        //When
         double expectedPerimeter = 6 + 3 + 4;
 
+        //When
+
+        double actualPerimeter = triangle.calculatePerimeter();
         //Then
-        assertEquals(expectedPerimeter, triangle.calculatePerimeter(), 0.0001);
+        assertEquals(expectedPerimeter, actualPerimeter, 0.0001);
     }
 }
